4596a4920ff2602fc54177ec51095e691f155c74
zer0conf <25299353+zer0conf@users.noreply.github.com>
2023-10-21 20:52:10 +0300

    Co-authored-by: Liene Beāte Arnicāne <LieniteB@users.noreply.github.com>

--- app/page.js
+++ app/page.js
@@ -3,93 +3,76 @@ import styles from './page.module.css'
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className={styles.main} id="home">
+      <nav className={styles.nav}>
+        <div className={styles.navButtons}>
+          <div>Shop</div>
+          <a href="#home"><div>Home</div></a>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <div className={styles.siteTitle}>
+          <img src="/images/logo-color.png" width="auto" height="40em"/>
+        </div>
+        <div>
+          <div className={styles.navButtons}>
+            <div>Technology</div>
+            <div>Research</div>
+            <a href="#about"><div>About</div></a>
+          </div>
+        </div>
+      </nav>
+      <div className={styles.container}>
+        <section className={styles.cardOne}>
+          <img src="/images/landing.png"/>
+          <div className={styles.introtext}>
+            <p className={styles.title}>ZenBand Bracelet</p>
+            <p className={styles.subtitle}>For daylight vigilance and nightime calm.</p>
+          </div>
+        </section>
+        <section className={styles.cardTwo}>
+          <div style={{display: "flex", backgroundColor: "lightgreen", borderRadius: "25px", padding: "2em", width: "100%", justifyContent: "space-between"}}>
+            <div className={styles.cardTwoItem}>
+              <h1>Manage your stress levels</h1>
+              <p>The bracelet uses gentle vibrations to calm you down and reduce cortisol levels.</p>
+            </div>
+            <div className={styles.cardTwoItem}>
+              <h1>Enhances the quality of your sleep</h1>
+              <p>The bracelet tracks your pulse and uses vibrations to slow down your heartbeat, making it easier to fall asleep.</p>
+            </div>
+            <div className={styles.cardTwoItem}>
+              <h1>Maintains your alertness</h1>
+              <p>The bracelet functions as both a tactile alarm clock and an alertness device, delivering shock-like vibrations to the arm whenever a out of place heartbeat slowdown is detected.</p>
+            </div>
+          </div>
+        </section>
+        <section className={styles.cardThree}>
+          <div>
+            <h1>Unparalleled versatility.</h1>
+          </div>
+          <div style={{display: "flex", justifyContent: "center", alignItems: "space-between", gap: "3em", minWidth: "20%"}}>
+            <ul>
+              <li>Intelligent sensors</li>
+              <li>Advanced vibromotors</li>
+              <li>Bluetooth 5.0</li>
+              <li>Wireless charging</li>
+              <li>Waterproof design</li>
+              <li>Customizable effects</li>
+            </ul>
+          </div>
+          <div style={{display: "flex", justifyContent: "center"}}>
+            <img src="/images/nightstand.png" width={"80%"} height="auto" style={{borderRadius: "25px", boxShadow: "5px 5px 15px rgba(0, 0, 0, 0.3)"}}/>
+          </div>
+        </section>
+        <section className={styles.cardFour}>
+          <p>Are you tired of feeling groggy and unproductive after a long drive or a boring lecture? Do you want to wake up feeling refreshed and energized? Look no further than ZenBand!</p>
+          <br/>
+          <p>ZenBand is designed to solve two polar ends of the same coin - it can both keep you awake, and promote faster sleep - all through the power of vibrotactile stimulation. The motors are gentle yet effective, providing a subtle nudge to help you stay awake and engaged.</p>
+        </section>
+        <section className={styles.cardFive} id="about">
+          <h2>About</h2>
+        </section>
       </div>
     </main>
   )
 }
+
+/* balts, melns, zaļs */
